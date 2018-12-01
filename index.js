3cd4eb2711f6a8a4861c7b2042415e8f79f8c9bf
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-12-01 14:58:45 -0500

    feat: use new `plugins` option and `@semantic-release/apm` plugin
    - Use `@semantic-release/apm` rather than custom code
    - Use new semantic-release `plugins` option
    - Update documentation
    - Remove test as the shareable config is purely static
    
    BREAKING CHANGE: require semantic-release@15.10.0 or above

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-const execa = require('execa');
-const SemanticReleaseError = require('@semantic-release/error');
-
 module.exports = {
-  verifyConditions: [
-    async () => {
-      if ((await execa('apm', ['-v'], {reject: false})).code !== 0) {
-        throw new SemanticReleaseError('The apm CLI must be installed.', 'ENOAPMCLI');
-      }
-    },
-    () => {
-      if (!process.env.ATOM_ACCESS_TOKEN) {
-        throw new SemanticReleaseError('The environment variable ATOM_ACCESS_TOKEN is required.', 'ENOAPMTOKEN');
-      }
-    },
-    {path: '@semantic-release/npm', npmPublish: false},
-    '@semantic-release/changelog',
-    '@semantic-release/git',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     '@semantic-release/github',
-  ],
-  prepare: [
     '@semantic-release/changelog',
-    {path: '@semantic-release/npm', npmPublish: false},
-    {path: '@semantic-release/git', message: `chore(release): \${nextRelease.version} [skip ci]`},
-  ],
-  publish: [
-    {path: '@semantic-release/exec', cmd: `apm publish --tag \${nextRelease.gitTag} 1>&2`},
-    '@semantic-release/github',
+    '@semantic-release/apm',
+    ['@semantic-release/git', {message: `chore(release): \${nextRelease.version} [skip ci]`}],
   ],
 };
